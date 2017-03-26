8bce5b9b4d025dc19bd5085e52a6e3bbf79e6e29
Sebastien Dery <sebastien.dery@lattice.io>
2017-03-25 22:31:16 -0700

    Sobering up AI by replacing its occurrence by ML related words

--- content.js
+++ content.js
@@ -1,4 +1,23 @@
-var images = document.getElementsByTagName('img');
-for (var i = 0, l = images.length; i < l; i++) {
-  images[i].src = 'http://placekitten.com/' + images[i].width + '/' + images[i].height;
+var synonyms = ["clever use of statistics", "machine learning", "multinomial logistic regression", "fully connected neural networks"];
+
+function replaceAll(str, find, replace) {
+  return str.replace(new RegExp(find, 'g'), replace);
 }
+
+function htmlreplace(a, b, element) {    
+    if (!element) element = document.body;    
+    var nodes = element.childNodes;
+    for (var n=0; n<nodes.length; n++) {
+        if (nodes[n].nodeType == Node.TEXT_NODE) {
+            var r = new RegExp(a, 'gi');
+            nodes[n].textContent = nodes[n].textContent.split(a).join(synonyms[Math.floor(Math.random() * synonyms.length)]); 
+        } else {
+            htmlreplace(a, b, nodes[n]);
+        }
+    }
+}
+
+htmlreplace('AI', 'machine learning');
+htmlreplace('A.I.', 'machine learning');
+htmlreplace('A.I', 'machine learning');
+htmlreplace('Artificial Intelligence', 'machine learning');
